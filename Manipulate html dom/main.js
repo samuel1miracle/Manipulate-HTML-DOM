231269ac954ec83edcadeb49dabf424b11582aaa
Samuel Miracle <88245584+samuel1miracle@users.noreply.github.com>
2021-09-23 13:08:09 +0100

    Add files via upload

---
+++ Manipulate html dom/main.js
@@ -0,0 +1,81 @@
+//ANSWER TO NUMBER 1
+
+document.getElementById('container');
+
+
+
+//ANSWER TO NUMBER 2
+
+document.getElementById('container');
+
+
+
+//ANSWER TO NUMBER 3
+
+document.getElementsByClassName('second');
+
+
+
+//ANSWER TO NUMBER 4
+
+
+let olSelect = document.getElementsByTagName('ol') 
+
+olSelect = document.querySelector('third')
+
+
+
+//ANSWER TO NUMBER 5
+
+
+document.getElementById('container').innerHTML += 'Hello!';
+
+
+
+
+//ANSWER TO NUMBER 6
+
+document.querySelector('.header').classList.add('footer');
+
+
+
+
+//ANSWER TO NUMBER 7
+
+document.querySelector('.footer').classList.remove('header');
+
+
+
+
+``
+// ANSWER TO NUMBER 8, 9 & 10
+
+let x = document.createElement('li');
+ let y = document.createTextNode('Four');
+ x.appendChild(y);
+let z = document.getElementsByTagName('ul');
+z = document.write('y')
+
+
+
+
+
+
+
+//ANSWER TO NUMBER 11
+
+
+let background = document.getElementsByTagName("ol")
+for (let i = 0; i < background.length; i++) {
+    background[i].style.backgroundColor = "green";
+}
+
+
+
+//ANSWER TO NUMBER 12
+
+let a = document.querySelector('.footer')
+a.classList.remove('footer');
+
+
+
